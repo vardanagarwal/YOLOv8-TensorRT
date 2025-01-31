2969b81abac6bb217e1f2d4e12afa6f04f426da3
vardanagarwal <vardanagarwal16@gmail.com>
2025-01-31 17:40:08 +0530

    do draw operation only if save enabled

--- infer-det.py
+++ infer-det.py
@@ -53,7 +53,6 @@ def main(args: argparse.Namespace) -> None:
             bgr = cv2.imread(str(image))
             if bgr is None:
                 continue
-            draw = bgr.copy()
             bgr, ratio, dwdh = letterbox(bgr, (W, H))
 
             dwdh_list.append(dwdh)
@@ -62,9 +61,11 @@ def main(args: argparse.Namespace) -> None:
             metadata.append({
                 'ratio': ratio,
                 # 'dwdh': dwdh,
-                'draw': draw,
+                # 'draw': draw,
             })
             if args.save:
+                draw = bgr.copy()
+                metadata[-1]['draw'] = draw
                 save_image = save_path / image.name
                 metadata[-1]['save_path'] = save_image
 
@@ -120,8 +121,8 @@ def main(args: argparse.Namespace) -> None:
             bboxes = result['bboxes'].cpu()
             scores = result['scores'].cpu()
             labels = result['labels'].cpu()
-            draw = result['metadata']['draw']
             if args.save:
+                draw = result['metadata']['draw']
                 save_path = result['metadata']['save_path']
 
             # Draw detections
